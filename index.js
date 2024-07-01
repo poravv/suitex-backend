c64d983db46e86df7bb9d085a7707148cc9728f4
explwi <andres.vera@tigo.net.py>
2024-07-01 09:03:46 -0400

    Commit nuevo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post("/send-email", async (req, res) => {
   const { to, subject, text } = req.body;
   try {
     let info = await emailHelper(to, subject, text);
-    res.status(200).send(`Email sent: ${info.response}`);
+    res.status(200).send(`Email sent: ${info}`);
   } catch (error) {
     res.status(500).send("Error sending email");
   }
